f019605ed9c8280aa69ef0f34adac8db795d3fcd
Igor Savichev <savichev@yandex-team.ru>
2018-11-06 17:21:47 +0500

    Добавили задачу

---
+++ lib.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Итератор по друзьям
+ * @constructor
+ * @param {Object[]} friends
+ * @param {Filter} filter
+ */
+function Iterator(friends, filter) {
+    console.info(friends, filter);
+}
+
+/**
+ * Итератор по друзям с ограничением по кругу
+ * @extends Iterator
+ * @constructor
+ * @param {Object[]} friends
+ * @param {Filter} filter
+ * @param {Number} maxLevel – максимальный круг друзей
+ */
+function LimitedIterator(friends, filter, maxLevel) {
+    console.info(friends, filter, maxLevel);
+}
+
+/**
+ * Фильтр друзей
+ * @constructor
+ */
+function Filter() {
+    console.info('Filter');
+}
+
+/**
+ * Фильтр друзей
+ * @extends Filter
+ * @constructor
+ */
+function MaleFilter() {
+    console.info('MaleFilter');
+}
+
+/**
+ * Фильтр друзей-девушек
+ * @extends Filter
+ * @constructor
+ */
+function FemaleFilter() {
+    console.info('FemaleFilter');
+}
+
+exports.Iterator = Iterator;
+exports.LimitedIterator = LimitedIterator;
+
+exports.Filter = Filter;
+exports.MaleFilter = MaleFilter;
+exports.FemaleFilter = FemaleFilter;
