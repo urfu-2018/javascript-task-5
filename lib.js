b96fc54921219f4fe9177bb4a84b8b82aa6f0349
JulieBlack666 <32227117+JulieBlack666@users.noreply.github.com>
2018-11-10 23:22:06 +0500

    try(сдать задание)

--- lib.js
+++ lib.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function getChosenFriendsList(friends, circlesCount = 100000) {
+    friends = friends.sort((x, y) => x.name.localeCompare(y.name));
+    let currentCircle = friends.filter(f => f.best);
+    let result = currentCircle;
+
+    for (let i = 1; i < circlesCount && result.length < friends.length; i++) {
+        let nextCircle = [];
+        currentCircle.forEach(element => {
+            element.friends.forEach(friendName => {
+                const friendObj = friends.find(e => e.name === friendName);
+                if (!result.includes(friendObj)) {
+                    nextCircle.push(friendObj);
+                    result.push(friendObj);
+                }
+            });
+        });
+
+        currentCircle = nextCircle.length ? nextCircle : friends.find(x => !result.includes(x));
+    }
+
+    return result;
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,7 +30,13 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+
+    this.orderedFriends = getChosenFriendsList(friends).filter(filter.isSutable);
+    this.next = () => this.orderedFriends.shift();
+    this.done = () => this.orderedFriends.length === 0;
 }
 
 /**
@@ -19,15 +48,20 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.orderedFriends = getChosenFriendsList(friends, maxLevel).filter(filter.isSutable);
 }
+
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.isSutable = function () {
+        return Boolean;
+    };
 }
 
 /**
@@ -36,7 +70,10 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    const maleFilter = Object.create(Filter.prototype);
+    maleFilter.isSutable = friend => friend.gender === 'male';
+
+    return maleFilter;
 }
 
 /**
@@ -45,7 +82,10 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    const femaleFilter = Object.create(Filter.prototype);
+    femaleFilter.isSutable = friend => friend.gender === 'female';
+
+    return femaleFilter;
 }
 
 exports.Iterator = Iterator;
