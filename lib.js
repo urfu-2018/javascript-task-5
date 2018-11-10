fd9fc64bc41b649751d7cb0544262f7f4ba034fe
Владимир Шелковкин <e3250@yandex.ru>
2018-11-10 16:51:45 +0500

    v1

--- lib.js
+++ lib.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const proto = {
+    inv: [],
+    index: 0,
+    done() {
+        return this.index >= this.inv.length;
+    },
+    next() {
+        return this.done() ? null : this.inv[this.index++];
+    }
+};
+
+Object.setPrototypeOf(Iterator.prototype, proto);
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -8,6 +24,10 @@
  */
 function Iterator(friends, filter) {
     console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('azaazazazaza');
+    }
+    this.inv = createAPair(friends).filter(f => filter.filterOut(f));
 }
 
 /**
@@ -20,6 +40,32 @@ function Iterator(friends, filter) {
  */
 function LimitedIterator(friends, filter, maxLevel) {
     console.info(friends, filter, maxLevel);
+    this.inv = createAPair(friends, maxLevel)
+        .filter(f => filter.filterOut(f));
+}
+
+function createAPair(friends, maxDepth = friends.length) {
+    var depth = 1;
+    var result = [];
+    var addFriends = friends.filter(f => f.best);
+    while (addFriends.length > 0 && depth++ <= maxDepth) {
+        result = result.concat(addFriends.sort((a, b) => a.name
+            .localeCompare(b.name)));
+        addFriends = friendsOfFriends(addFriends, result)
+            .map(name => friends.find(friend => friend.name === name));
+    }
+
+    return result;
+}
+
+function friendsOfFriends(addFriends, listInvFriends) {
+    var result = [];
+    var listFriends = listInvFriends.map(f => f.name);
+    for (var i = 0; i < addFriends.length; i++) {
+        result.push(...addFriends[i].friends.filter(f => !listFriends.includes(f)));
+    }
+
+    return result;
 }
 
 /**
@@ -28,6 +74,7 @@ function LimitedIterator(friends, filter, maxLevel) {
  */
 function Filter() {
     console.info('Filter');
+    this.filterOut = () => true;
 }
 
 /**
@@ -37,6 +84,7 @@ function Filter() {
  */
 function MaleFilter() {
     console.info('MaleFilter');
+    this.filterOut = (friend) => friend.gender === 'male';
 }
 
 /**
@@ -46,6 +94,7 @@ function MaleFilter() {
  */
 function FemaleFilter() {
     console.info('FemaleFilter');
+    this.filterOut = (friend) => friend.gender === 'female';
 }
 
 exports.Iterator = Iterator;
