bddce528644261b3be609cc24a761e1441106952
IIaKyJIuH <38308131+IIaKyJIuH@users.noreply.github.com>
2018-11-10 12:22:07 +0500

    version 1

--- lib.js
+++ lib.js
@@ -7,7 +7,22 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+    this.guests = getInvitedFriends(friends, filter);
+    this.k = -1;
+    this.done = () => {
+        if (this.k === this.guests.length - 1 ||
+            this.guests.length === 0) {
+            return true;
+        }
+
+        return false;
+    };
+    this.next = () => {
+        return this.done() ? null : this.guests[++(this.k)];
+    };
 }
 
 /**
@@ -19,7 +34,47 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.guests = getInvitedFriends(friends, filter, maxLevel);
+}
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+
+/**
+* Подготовка списка друзей, которых Билли пригласит на свадьбу.
+* @param {Object[]} allPeople – список всех людей из блокнота Билли.
+* @param {Filter} genderFilter – фильтр по полу.
+* @param {Number} maxLevel – максимальный круг друзей, если не задан - смотрим всех.
+* @returns {Object[]} – отсортированный список приглашаемых на свадьбу друзей.
+*/
+function getInvitedFriends(allPeople, genderFilter, maxLevel = Infinity) {
+    let invitedFriends = [];
+    const sortNames = (first, second) => first.name.localeCompare(second.name);
+    let currentLevelFriends = allPeople.filter(friend => friend.best).sort(sortNames);
+    while (maxLevel > 0 && currentLevelFriends.length > 0) {
+        invitedFriends = invitedFriends.concat(currentLevelFriends);
+        currentLevelFriends = getNextLevel(currentLevelFriends, allPeople, invitedFriends)
+            .sort(sortNames);
+        maxLevel--;
+    }
+
+    return invitedFriends.filter(genderFilter.weed);
+}
+
+/**
+* Получение следующего круга друзей.
+* @param {Object[]} curLevel – текущий круг приглашаемых друзей.
+* @param {Object[]} originArray – изначальный список всех людей.
+* @param {Number} accumulator – все те люди, которых мы уже "накопили" ранее.
+* @returns {Object[]} – список следующего круга уникальных друзей.
+*/
+function getNextLevel(curLevel, originArray, accumulator) {
+    const reducer = (newArr, person) => newArr.concat(person.friends);
+
+    return Array.from(new Set(curLevel
+        .reduce(reducer, [])))
+        .map(each => originArray.find(person => each === person.name))
+        .filter(person => !accumulator.includes(person));
 }
 
 /**
@@ -28,6 +83,8 @@ function LimitedIterator(friends, filter, maxLevel) {
  */
 function Filter() {
     console.info('Filter');
+
+    this.weed = () => true;
 }
 
 /**
@@ -39,6 +96,12 @@ function MaleFilter() {
     console.info('MaleFilter');
 }
 
+MaleFilter.prototype = Object.create(Filter.prototype, {
+    weed: {
+        value: (friend) => friend.gender === 'male'
+    }
+});
+
 /**
  * Фильтр друзей-девушек
  * @extends Filter
@@ -47,6 +110,13 @@ function MaleFilter() {
 function FemaleFilter() {
     console.info('FemaleFilter');
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype, {
+    weed: {
+        value: (friend) => friend.gender === 'female'
+    }
+});
+
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
