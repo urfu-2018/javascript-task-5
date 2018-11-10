c8cae65c93a2b1dc6a10badb3bc0bf2d816a3dff
Alex768 <vortigont768@inbox.ru>
2018-11-10 22:26:57 +0500

    initial

--- lib.js
+++ lib.js
@@ -1,13 +1,95 @@
 'use strict';
 
 /**
+ * Возвращает список друзей, удовлятворяющих условиям фильтра
+ * @param {Object[]} friends - список всех друзей
+ * @param {Filter} filter - фильтр
+ * @param {Number} levels - максимальный круг друзей
+ * @returns {Object[]}
+ */
+function getFriends(friends, filter, levels = Number.MAX_SAFE_INTEGER) {
+    // друзья на 1 круге (лучшие друзья)
+    let levelFriends = friends.filter(friend => friend.best);
+    // приглашенные лучшие друзья
+    let invited = levelFriends
+        .filter(friend => filter.isAcceptableFriend(friend))
+        .sort(friendsNamesCompare);
+    while (levels > 1) {
+        // имена друзей на втором круге (друзья лучших друзей; на 3 круге и т.д. аналогично)
+        const NEXT_LEVEL_NAMES = getNextLevelNames(levelFriends);
+        // список друзей на втором круге в виде массива объектов (на 3 круге и т.д. аналогично)
+        levelFriends = getLevelFriends(friends, NEXT_LEVEL_NAMES, invited);
+        // список приглашенных друзей на втором круге (на 3 круге и т.д. аналогично)
+        const NEXT_LEVEL_INVITED = levelFriends.filter(friend => {
+            return filter.isAcceptableFriend(friend);
+        }).sort(friendsNamesCompare);
+        // если на втором круге приглашенных - выходим из цикла (на 3 круге и т.д. аналогично)
+        if (!NEXT_LEVEL_INVITED.length) {
+            break;
+        }
+        // добавляем к списку всех приглашенных (на 3 круге и т.д. аналогично)
+        invited = invited.concat(NEXT_LEVEL_INVITED);
+        levels--;
+    }
+
+    return invited;
+}
+
+/**
+ * Comparator для сортировки друзей
+ * @param {Object} firstFriend
+ * @param {Object} secondFriend
+ * @returns {Number}
+ */
+function friendsNamesCompare(firstFriend, secondFriend) {
+    return firstFriend.name.localeCompare(secondFriend.name);
+}
+
+/**
+ * Возвращает массив имен друзей для следующего круга
+ * @param {Object[]} levelFriends - список друзей на текущем круге (только на одном круге)
+ * @returns {String[]}
+ */
+function getNextLevelNames(levelFriends) {
+    return levelFriends.reduce((names, friend) => {
+        friend.friends.forEach(name => {
+            if (!names.includes(name)) {
+                names.push(name);
+            }
+        });
+
+        return names;
+    }, []);
+}
+
+/**
+ * Возвращает список друзей на следующем круге на основе списка имен друзей следующего
+ * круга (друзья друзей) и списка уже приглашенных друзей
+ * @param {Object[]} friends - список всех друзей
+ * @param {String[]} names - список имен друзей на следующем круге (друзья друзей)
+ * @param {Object[]} invited - список приглашенных друзей
+ * @returns {Object[]}
+ */
+function getLevelFriends(friends, names, invited) {
+    return friends.filter(friend => {
+        return names.includes(friend.name) && !invited.some(invitedFriend => {
+            return invitedFriend.name === friend.name;
+        });
+    });
+}
+
+/**
  * Итератор по друзьям
  * @constructor
  * @param {Object[]} friends
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('specified filter is not an instance of Filter');
+    }
+    this.friendIndex = 0;
+    this.friends = getFriends(friends, filter);
 }
 
 /**
@@ -19,7 +101,11 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.super(friends, filter);
+    if (!(typeof maxLevel === 'number' && maxLevel > 0)) {
+        maxLevel = 1;
+    }
+    this.friends = getFriends(friends, filter, maxLevel);
 }
 
 /**
@@ -27,7 +113,7 @@ function LimitedIterator(friends, filter, maxLevel) {
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.acceptableGender = null;
 }
 
 /**
@@ -36,7 +122,7 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.acceptableGender = 'male';
 }
 
 /**
@@ -45,8 +131,53 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.acceptableGender = 'female';
 }
+
+/**
+ * Проверяет, походит ли `friend` под условия фильтрации
+ * @param {Object] friend - друг из списка всех друзей
+ * @returns {boolean}
+ */
+Filter.prototype.isAcceptableFriend = function (friend) {
+    if (!this.acceptableGender) {
+        return true;
+    }
+
+    return this.acceptableGender === friend.gender;
+};
+
+/**
+ * Проверяет, закончена ли работа итератора
+ * @returns {boolean}
+ */
+Iterator.prototype.done = function () {
+    return this.friendIndex >= this.friends.length;
+};
+
+/**
+ * Возвращает следующий элемент итерации
+ * @returns {Object|null}
+ */
+Iterator.prototype.next = function () {
+    if (this.done()) {
+        return null;
+    }
+
+    return this.friends[this.friendIndex++];
+};
+
+/**
+ * Фильтрам в прототип записываем прототип базового фильтра
+ */
+MaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype = Object.create(Filter.prototype);
+
+/**
+ * Итератору с ограничением в прототип записываем прототип базового итератора
+ */
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.super = Iterator;
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
