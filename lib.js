176aaa3998769b5cf422e6f2a40c7017e9654edf
predatorfromekb <stepanus97@yandex.ru>
2018-11-12 01:03:56 +0500

    задача

--- lib.js
+++ lib.js
@@ -7,8 +7,23 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    this.friendsWrapper = filterAndOrder(friends, filter);
 }
+
+Object.assign(Iterator.prototype, {
+    done() {
+        const friendsWrapper = this.friendsWrapper;
+
+        return friendsWrapper.friends.length === friendsWrapper.current;
+    },
+    next() {
+        const friendsWrapper = this.friendsWrapper;
+
+        return this.done()
+            ? null
+            : friendsWrapper.friends[friendsWrapper.current++].object;
+    }
+});
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,16 +34,25 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.friendsWrapper = filterAndOrder(friends, filter, maxLevel);
 }
+
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.gender = 'all';
 }
+
+Object.assign(Filter.prototype, {
+    apply(friend) {
+        return friend.gender === this.gender ||
+            this.gender === 'all';
+    }
+});
 
 /**
  * Фильтр друзей
@@ -36,8 +60,10 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.gender = 'male';
 }
+
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
 
 /**
  * Фильтр друзей-девушек
@@ -45,7 +71,70 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.gender = 'female';
+}
+
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
+
+function filterAndOrder(friends, filter, maxLevel = Number.POSITIVE_INFINITY) {
+    let temp = friends
+
+        // .sort(friendsComparer)
+        .map(friend => {
+            return {
+                object: friend,
+                level: friend.best ? 1 : Number.POSITIVE_INFINITY
+            };
+        });
+
+    temp = bubbleSort(temp)
+        .filter(friend => filter.apply(friend.object))
+        .filter(friend => friend.level <= maxLevel);
+
+    return {
+        current: 0,
+        friends: temp
+    };
+}
+
+function friendsComparerInternal(friendA, friendB) {
+    return !(friendA.best === friendB.best
+        ? friendA.name < friendB.name
+        : friendA.best);
+}
+
+function bubbleSort(friends) {
+    for (let i = 0; i < friends.length; i++) {
+        for (let j = i + 1; j < friends.length; j++) {
+            bubbleSortInternal(friends, i, j);
+        }
+    }
+
+    return friends;
+}
+
+function bubbleSortInternal(friends, i, j) {
+    const friendA = friends[i];
+    const friendB = friends[j];
+
+    if (friendA.level === friendB.level) {
+        if (friendsComparerInternal(friendA.object, friendB.object)) {
+            swap(friends, i, j);
+        }
+    } else if (friendA.level < friendB.level + 1) {
+        if (friendA.object.friends.includes(friendB.object.name)) {
+            friendB.level = friendA.level + 1;
+        }
+    } else if (friendA.level > friendB.level) {
+        swap(friends, i, j);
+    }
+
+}
+
+function swap(friends, i, j) {
+    const temp = friends[i];
+    friends[i] = friends[j];
+    friends[j] = temp;
 }
 
 exports.Iterator = Iterator;
