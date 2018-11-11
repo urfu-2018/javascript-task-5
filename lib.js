1cc83d84def0e41450accb0674b7b82d46718e41
Leoltron <leoole5@gmail.com>
2018-11-11 16:20:06 +0500

    Initial commit

--- lib.js
+++ lib.js
@@ -1,13 +1,113 @@
 'use strict';
 
+class ArrayIterator {
+    constructor(array) {
+        this._array = array;
+        this._length = array.length;
+        this._nextIndex = 0;
+    }
+
+    next() {
+        return this.done() ? null : this._array[this._nextIndex++];
+    }
+
+    done() {
+        return this._nextIndex === this._length;
+    }
+}
+
+class SimpleFilter {
+
+    check() {
+        return true;
+    }
+
+    filterAll(array) {
+        const filtered = [];
+        for (const element of array) {
+            if (this.check(element)) {
+                filtered.push(element);
+            }
+        }
+
+        return filtered;
+    }
+}
+
+class GenderFilter extends SimpleFilter {
+    constructor(gender) {
+        super();
+        this._gender = gender;
+    }
+
+    check(friend) {
+        return friend.gender === this._gender;
+    }
+}
+
+function addAll(set, iterable) {
+    for (const element of iterable) {
+        set.add(element);
+    }
+}
+
+class FriendPicker {
+    constructor(friends) {
+        this._friends = friends.slice();
+        this._friends.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    getFriends(depth) {
+        const friends = this._friends.slice();
+        const pickedFriends = [];
+        let currentStepFriends = new Set(this._getBestFriends());
+        for (let i = 0; i < depth && friends.length > 0; i++) {
+            currentStepFriends = FriendPicker._performFriendPickStep(friends,
+                currentStepFriends, pickedFriends);
+        }
+
+        return pickedFriends;
+    }
+
+    static _performFriendPickStep(friends, currentStepFriends, pickedFriends) {
+        let nextStepFriends = new Set();
+        for (let j = 0; j < friends.length; j++) {
+            const friend = friends[j];
+            if (currentStepFriends.has(friend.name)) {
+                pickedFriends.push(friend);
+                friends.splice(j, 1);
+                j--;
+                addAll(nextStepFriends, friend.friends);
+            }
+        }
+
+        return nextStepFriends;
+    }
+
+    _getBestFriends() {
+        const bestFriends = [];
+        for (const friend of this._friends) {
+            if (friend.best) {
+                bestFriends.push(friend.name);
+            }
+        }
+
+        return bestFriends;
+    }
+
+}
+
 /**
  * Итератор по друзьям
  * @constructor
  * @param {Object[]} friends
- * @param {Filter} filter
+ * @param {SimpleFilter} filter
  */
 function Iterator(friends, filter) {
     console.info(friends, filter);
+    const pickedFriends = new FriendPicker(friends).getFriends(Number.MAX_SAFE_INTEGER);
+
+    return new ArrayIterator(filter.filterAll(pickedFriends));
 }
 
 /**
@@ -15,11 +115,14 @@ function Iterator(friends, filter) {
  * @extends Iterator
  * @constructor
  * @param {Object[]} friends
- * @param {Filter} filter
+ * @param {SimpleFilter} filter
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
     console.info(friends, filter, maxLevel);
+    const pickedFriends = new FriendPicker(friends).getFriends(maxLevel);
+
+    return new ArrayIterator(filter.filterAll(pickedFriends));
 }
 
 /**
@@ -28,6 +131,8 @@ function LimitedIterator(friends, filter, maxLevel) {
  */
 function Filter() {
     console.info('Filter');
+
+    return new SimpleFilter();
 }
 
 /**
@@ -37,6 +142,8 @@ function Filter() {
  */
 function MaleFilter() {
     console.info('MaleFilter');
+
+    return new GenderFilter('male');
 }
 
 /**
@@ -46,6 +153,8 @@ function MaleFilter() {
  */
 function FemaleFilter() {
     console.info('FemaleFilter');
+
+    return new GenderFilter('female');
 }
 
 exports.Iterator = Iterator;
