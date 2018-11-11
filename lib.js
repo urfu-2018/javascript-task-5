855e45ecb642ece17742d040c4856e02b1f4fdce
Resinchen <alex1410alex@gmail.com>
2018-11-11 17:15:42 +0500

    Решение задачи

--- lib.js
+++ lib.js
@@ -4,11 +4,54 @@
  * Итератор по друзьям
  * @constructor
  * @param {Object[]} friends
- * @param {Filter} filter
+ * @param {Filter} ffilter
  */
-function Iterator(friends, filter) {
-    console.info(friends, filter);
+function Iterator(friends, ffilter) {
+    if (!(ffilter instanceof Filter)) {
+        throw new TypeError('filter must be instance Filter');
+    }
+
+    this.ffriends = this.getCircles(friends, this.maxLevel)
+        .filter(ffilter.check);
 }
+
+function getNextCircle(currentCircle, circles, friends) {
+    const nextCircle = [];
+
+    currentCircle.forEach(curFriend =>
+        curFriend.friends.forEach(name => {
+            const friend = friends.find(person => person.name === name);
+            if (!circles.includes(friend)) {
+                nextCircle.push(friend);
+            }
+        })
+    );
+
+    return [...new Set(nextCircle)].sort((a, b) => a.name.localeCompare(b.name));
+}
+
+Iterator.prototype = {
+    constructor: Iterator,
+    done() {
+        return this.ffriends.length === 0;
+    },
+    next() {
+        return this.done() ? null : this.ffriends.shift();
+    },
+    getCircles(friends, maxLevel = Infinity) {
+        let currentCircle = friends.filter(friend => friend.best)
+            .sort((a, b) => a.name.localeCompare(b.name));
+        const circles = [];
+
+        while (currentCircle.length > 0 && maxLevel > 0) {
+            circles.push(...currentCircle);
+            currentCircle = getNextCircle(currentCircle, circles, friends);
+            maxLevel--;
+        }
+
+        return circles;
+    }
+};
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,8 +62,12 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.maxLevel = maxLevel;
+    Iterator.call(this, friends, filter);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
@@ -29,6 +76,8 @@ function LimitedIterator(friends, filter, maxLevel) {
 function Filter() {
     console.info('Filter');
 }
+
+Filter.prototype.check = () => true;
 
 /**
  * Фильтр друзей
@@ -39,6 +88,15 @@ function MaleFilter() {
     console.info('MaleFilter');
 }
 
+MaleFilter.prototype = Object.create(Filter.prototype, {
+    constructor: {
+        value: MaleFilter
+    },
+    check: {
+        value: friend => friend.gender === 'male'
+    }
+});
+
 /**
  * Фильтр друзей-девушек
  * @extends Filter
@@ -47,6 +105,15 @@ function MaleFilter() {
 function FemaleFilter() {
     console.info('FemaleFilter');
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype, {
+    constructor: {
+        value: FemaleFilter
+    },
+    check: {
+        value: friend => friend.gender === 'female'
+    }
+});
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
