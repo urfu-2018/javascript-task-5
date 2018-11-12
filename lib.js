04693b1a6262b78e567284226c98f12ece8354a3
Solodovnikova Ekaterina <colo18@yandex.ru>
2018-11-12 22:12:56 +0500

    total rewriting

--- lib.js
+++ lib.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,62 +8,22 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    var priorityFriends = addPriorities(friends);
-    priorityFriends.sort(friendsSortFunction);
-    priorityFriends = removePriorities(priorityFriends);
+    Object.setPrototypeOf(this, {
+        currentIndex: 0,
+        listOfGuests: [],
+        next() {
+            return this.done() ? null : this.listOfGuests[this.currentIndex++];
+        },
+        done() {
+            return this.currentIndex >= this.listOfGuests.length;
+        }
+    });
     if (filter instanceof Filter) {
-
-        return {
-            currentIndex: 0,
-            friendsIndexes: undefined,
-            sortedFriends: priorityFriends,
-            next: function () {
-                return (this.done()) ? null
-                    : this.sortedFriends[this.friendsIndexes[this.currentIndex++]];
-            },
-
-            done: function () {
-                if (this.friendsIndexes === undefined) {
-                    const bestFriendsIndexes = new Set(getBestFriendsIndexes(this.sortedFriends));
-                    this.friendsIndexes = filterIndexes(filter, bestFriendsIndexes,
-                        this.sortedFriends);
-
-                    return this.currentIndex === this.friendsIndexes.length;
-                } else if (this.friendsIndexes.length === 0) {
-                    return true;
-                }
-
-                return this.currentIndex === this.friendsIndexes.length;
-            }
-        };
+        this.listOfGuests = chooseFriends(friends).filter(friend => filter.isAppropriate(friend));
+        this.currentIndex = 0;
+    } else {
+        throw new TypeError();
     }
-
-    throw new TypeError();
-}
-
-function removePriorities(friends) {
-    friends.forEach(function (t) {
-        delete t.priority;
-    });
-
-    return friends;
-}
-
-function addPriorities(friends) {
-    const copyOfFriends = friends.map(b => ({ ...b }));
-    const bestFriends = getBestFriendsIndexes(friends, 1);
-    copyOfFriends.forEach(function (t) {
-        var bestFriendsCounter = 0;
-        t.friends.forEach(function (value) {
-            var friendIndex = findFriendIndex(friends, value);
-            if (bestFriends.indexOf(friendIndex) !== -1) {
-                bestFriendsCounter++;
-            }
-        });
-        t.priority = bestFriendsCounter;
-    });
-
-    return copyOfFriends;
 }
 
 /**
@@ -75,83 +36,30 @@ function addPriorities(friends) {
  */
 function LimitedIterator(friends, filter, maxLevel) {
     Object.setPrototypeOf(this, new Iterator(friends, filter));
-    if (maxLevel <= 0) {
-        this.friendsIndexes = [];
-
-        return this;
-    }
-    const bestFriendsIndexes = new Set(getBestFriendsIndexes(this.sortedFriends, maxLevel));
-    this.friendsIndexes = filterIndexes(filter, bestFriendsIndexes, this.sortedFriends);
-
-    return this;
+    this.listOfGuests = chooseFriends(friends, maxLevel)
+        .filter(friend => filter.isAppropriate(friend));
 }
 
-function filterIndexes(filter, bestFriendsIndexes, friends) {
-    var result = [];
-    bestFriendsIndexes.forEach(function (t) {
-        if (filter.isAvailable(friends[t])) {
-            result.push(t);
-        }
-    });
+function chooseFriends(friends, maxLevel = friends.length) {
+    var selection = [];
+    var currentLevel = 1;
+    var currentLevelFriends = friends.filter(friend => friend.best);
 
-    return result;
-}
+    while (currentLevelFriends.length > 0 && currentLevel <= maxLevel) {
+        selection.push(...currentLevelFriends
+            .sort((first, second) => first.name.localeCompare(second.name))
+        );
 
-function getBestFriendsIndexes(friends, maxLevel) {
-    const bestFriendsIndexes = [];
-    friends.forEach(function (i, index) {
-        if (i.best) {
-            bestFriendsIndexes.push(index);
-        }
-    });
-    if (maxLevel <= 1) {
-        return bestFriendsIndexes;
-    }
-
-    // return collectFriends(friends, maxLevel);
-
-    return goThrough(bestFriendsIndexes, friends, maxLevel);
-}
-
-function goThrough(bestFriendsIndexes, friends, maxLevel = friends.length) {
-    // bestfriends indexes
-    // maxlevel
-    // friends all
-
-    var queue = Object.assign([], bestFriendsIndexes);
-    var currentStep = 0;
-    var amountOnLayer = bestFriendsIndexes.length;
-    var visitedNodes = [];
-    while (currentStep !== maxLevel - 1) {
-        var i = 0;
-        while (i !== amountOnLayer && queue.length > 0) {
-            var firstEl = queue.shift();
-            visitedNodes.push(firstEl);
-            friends[firstEl].friends.forEach(function (t) {
-                var friendIndex = findFriendIndex(friends, t);
-                if (friendIndex !== -1 && queue.indexOf(friendIndex) === -1 &&
-                    visitedNodes.indexOf(friendIndex) === -1) {
-                    queue.push(friendIndex);
-                }
+        currentLevelFriends = currentLevelFriends
+            .reduce((acc, friend) => acc.concat(friend.friends), [])
+            .map(name => friends.find(friend => friend.name === name))
+            .filter(function (friend, index, arr) {
+                return !(selection.includes(friend) || arr.indexOf(friend) !== index);
             });
-            i++;
-        }
-        amountOnLayer = queue.length;
-        currentStep++;
+        currentLevel++;
     }
 
-    return visitedNodes.concat(queue);
-}
-
-function findFriendIndex(friends, friendName) {
-    var result = -1;
-    friends.forEach(function (t, index) {
-        if (t.name === friendName) {
-            result = index;
-        }
-    });
-
-    return result;
+    return selection;
 }
 
 /**
@@ -178,7 +86,7 @@ function Filter() {
 function MaleFilter() {
 
     Object.setPrototypeOf(this, new Filter());
-    Object.defineProperty(this, 'isAvailable', {
+    Object.defineProperty(this, 'isAppropriate', {
         value: function (a) {
             return a.gender === 'male';
         }
@@ -195,28 +103,13 @@ function MaleFilter() {
  */
 function FemaleFilter() {
     Object.setPrototypeOf(this, new Filter());
-    Object.defineProperty(this, 'isAvailable', {
+    Object.defineProperty(this, 'isAppropriate', {
         value: function (a) {
             return a.gender === 'female';
         }
     });
 
     return this;
-}
-
-function friendsSortFunction(firstFriend, secondFriend) {
-    const isSecondIsBest = (secondFriend.best) ? 1 : 0;
-    const isFirstIsBest = (firstFriend.best) ? 1 : 0;
-    const isBest = isSecondIsBest - isFirstIsBest;
-    if (isBest) {
-        return isBest;
-    }
-    if (firstFriend.best) {
-        return firstFriend.name.localeCompare(secondFriend.name, 'kn');
-    }
-
-    return firstFriend.name.localeCompare(secondFriend.name, 'kn') &&
-        firstFriend.priority <= secondFriend.priority;
 }
 
 exports.Iterator = Iterator;
