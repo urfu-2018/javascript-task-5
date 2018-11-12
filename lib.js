6487f059ea8f1bb3d7bcab6cf74c58878d9cbfea
evilPaprika <ivil.paprika@gmail.com>
2018-11-13 00:15:00 +0500

    изменил условие в назначении круга

--- lib.js
+++ lib.js
@@ -6,7 +6,9 @@ function assignCirclesFromBestFriend(bestFriend, friends) {
     while (queue.length) {
         let friend = queue.shift();
         let friendsOfFriend = friends.filter(
-            f => friend.friends.includes(f.name) && !(f.circle <= friend.circle)
+            f =>
+                friend.friends.includes(f.name) &&
+                (typeof f.circle === 'undefined' || f.circle > friend.circle)
         );
         queue.push(...friendsOfFriend);
         friendsOfFriend.forEach(f => (f.circle = friend.circle + 1));
