4430b800813740993ba85bad7d6a962cf15d0400
g0djan <g0dj4n@gmail.com>
2018-11-13 02:56:23 +0500

    1

--- lib.js
+++ lib.js
@@ -1,5 +1,46 @@
 'use strict';
 
+function getBestFriends(friends) {
+    return friends
+        .filter(friend => friend.best)
+        .sort((a, b) => a.name > b.name)
+        .map(function (friend) {
+            friend.priority = 0;
+
+            return friend;
+        });
+}
+
+function addNextLevel(current, friends, visited, queue) {
+    for (let i = 0; i < current.friends.length; ++i) {
+        const friendName = current.friends[i];
+        const friend = friends.find(fr => fr.name === friendName);
+        if (!friend.hasOwnProperty('priority')) {
+            friend.priority = current.priority + 1;
+        }
+        if (!visited.has(friendName)) {
+            queue.push(friend);
+            visited.add(friendName);
+        }
+    }
+}
+
+function* bfs(friends) {
+    const queue = getBestFriends(friends);
+    const visited = new Set(queue.map(friend => friend.name));
+    let currentPriority = 0;
+    while (queue.length > 0) {
+        if (currentPriority !== queue[0].priority) {
+            queue.sort((a, b) => a.name > b.name);
+            ++currentPriority;
+        }
+        const current = queue.shift();
+        addNextLevel(current, friends, visited, queue);
+        yield current;
+    }
+
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -8,6 +49,33 @@
  */
 function Iterator(friends, filter) {
     console.info(friends, filter);
+    if (filter.prototype.name !== 'Filter') {
+        throw new TypeError();
+    }
+    const iter = bfs(friends);
+    let next = iter.next();
+
+    return {
+        current: function () {
+            return next;
+        },
+        done: function () {
+            while (!next.done && !filter.filter(next.value)) {
+                next = iter.next();
+            }
+
+            return next.done;
+        },
+        next: function () {
+            while (!next.done && !filter.filter(next.value)) {
+                next = iter.next();
+            }
+            let value = next.done ? null : next.value;
+            next = iter.next();
+
+            return value;
+        }
+    };
 }
 
 /**
@@ -20,6 +88,11 @@ function Iterator(friends, filter) {
  */
 function LimitedIterator(friends, filter, maxLevel) {
     console.info(friends, filter, maxLevel);
+
+    return new Iterator(friends, {
+        filter: fr => filter.filter(fr) && fr.priority < maxLevel,
+        prototype: Filter
+    });
 }
 
 /**
@@ -28,6 +101,7 @@ function LimitedIterator(friends, filter, maxLevel) {
  */
 function Filter() {
     console.info('Filter');
+    this.filter = () => true;
 }
 
 /**
@@ -37,6 +111,8 @@ function Filter() {
  */
 function MaleFilter() {
     console.info('MaleFilter');
+    this.prototype = Filter;
+    this.filter = friend => friend.gender === 'male';
 }
 
 /**
@@ -46,6 +122,8 @@ function MaleFilter() {
  */
 function FemaleFilter() {
     console.info('FemaleFilter');
+    this.prototype = Filter;
+    this.filter = friend => friend.gender === 'female';
 }
 
 exports.Iterator = Iterator;
