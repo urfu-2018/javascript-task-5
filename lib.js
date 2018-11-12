dc339c5f6365d61c8c2433fbc39acfeebfc9d24e
Корабельникова Татьяна <tk-imi@yandex.ru>
2018-11-12 17:24:41 +0500

    Решение задачи

--- lib.js
+++ lib.js
@@ -7,11 +7,23 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError();
+    }
+
+    this.collection = prepareFriendsList(friends, filter);
 }
 
+Iterator.prototype.done = function () {
+    return this.collection.length === 0;
+};
+
+Iterator.prototype.next = function () {
+    return this.done() ? null : this.collection.shift();
+};
+
 /**
- * Итератор по друзям с ограничением по кругу
+ * Итератор по друзьям с ограничением по кругу
  * @extends Iterator
  * @constructor
  * @param {Object[]} friends
@@ -19,16 +31,20 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.collection = prepareFriendsList(friends, filter, maxLevel);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
+LimitedIterator.prototype.constructor = LimitedIterator;
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.getAppropriateItem = () => true;
 }
+
 
 /**
  * Фильтр друзей
@@ -36,8 +52,11 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.getAppropriateItem = (person) => person.gender === 'male';
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
+MaleFilter.prototype.constructor = MaleFilter;
 
 /**
  * Фильтр друзей-девушек
@@ -45,9 +64,48 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.getAppropriateItem = (person) => person.gender === 'female';
 }
 
+FemaleFilter.prototype = Object.create(Filter.prototype);
+FemaleFilter.prototype.constructor = FemaleFilter;
+
+function prepareFriendsList(friends, filter, maxLevel = friends.length) {
+    let invitedFriends = friends
+        .filter(friend => friend.best !== undefined && friend.best === true)
+        .sort(alphabetSort);
+
+    let nextLevelFriends = getNextLevelFriends(invitedFriends, invitedFriends, friends);
+    let level = 1;
+
+    while (nextLevelFriends.length !== 0 && level < maxLevel) {
+        invitedFriends = invitedFriends.concat(nextLevelFriends);
+        nextLevelFriends = getNextLevelFriends(nextLevelFriends, invitedFriends, friends);
+        ++level;
+    }
+
+    return invitedFriends.filter(friend => filter.getAppropriateItem(friend));
+}
+
+function getNextLevelFriends(currentFriends, invitedFriends, phonebook) {
+    let nextLevelFriends = [];
+    currentFriends.forEach(friend => {
+        friend.friends.forEach(nextLevelFriend => {
+            if (!nextLevelFriends.includes(nextLevelFriend)) {
+                nextLevelFriends.push(nextLevelFriend);
+            }
+        });
+    });
+
+    return nextLevelFriends
+        .filter(friend => invitedFriends.every(invitedFriend => invitedFriend.name !== friend))
+        .map(friend => phonebook.find(friendInPhonebook => friendInPhonebook.name === friend))
+        .sort(alphabetSort);
+}
+
+function alphabetSort(first, secont) {
+    return first.name.localeCompare(secont.name);
+}
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
 
