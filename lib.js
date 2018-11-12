c0e35b47434074afa843753a7f91c50ae7931c8e
elena-i-m98 <43844991+elena-i-m98@users.noreply.github.com>
2018-11-12 15:09:25 +0500

    решение

--- lib.js
+++ lib.js
@@ -7,8 +7,27 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!filter instanceof Filter) {
+        throw new TypeError();
+    }
+    this.weddingGuests = getInvitedFriends(friends, filter);
+    this.index = 0;
+    this.done = () => {
+        return index >= this.weddingGuests.length; 
+    };
+    this.next = () => {
+        if (this.done) {
+            return null;
+        } else {
+            return this.weddingGuests[this.index++];
+        }
+    };
 }
+
+Object.setPrototypeOf(LimitedIterator.prototype, Iterator.prototype);
+Object.setPrototypeOf(FemaleFilter.prototype, Filter.prototype);
+Object.setPrototypeOf(MaleFilter.prototype, Filter.prototype);
+
 
 /**
  * Итератор по друзям с ограничением по кругу
@@ -19,7 +38,7 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    this.weddingGuests = getInvitedFriends(friends, filter, maxLevel);
 }
 
 /**
@@ -27,7 +46,7 @@ function LimitedIterator(friends, filter, maxLevel) {
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    return this.isSuitable = () => true;
 }
 
 /**
@@ -36,7 +55,7 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.isSuitable = (friend) => friend.gender === 'male';
 }
 
 /**
@@ -45,7 +64,40 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.isSuitable = (friend) => friend.gender === 'female';
+}
+
+/**
+ * Получение списка друзей, которые будут приглашены на свадьбу
+ * @param {Object[]} AllFriends - список всех друзей
+ * @param {Filter} filter
+ * @param {Number} maxLevel - максимальный круг друзей
+ * @returns {Object[]} - отсортированный массив,
+ * кого Аркадий пригласит на свадьбу
+ */
+function getInvitedFriends(friends, filter, maxLevel = Infinity) {
+    let  invited = [];
+    let currentLevelList = friends.filter(friend => friend.best)
+        .sort((a,b) => a.name.localeCompare(b.name));
+    while (maxLevel > 0 && currentLevelList.length > 0) {
+        invited = invited.concat(currentLevelList);
+        currentLevelList = getNextLefelFriends(currentLevelList, invited, friends);
+        maxLevel--;
+    }
+
+    return invited.filter(filter);
+}
+
+function getNextLefelFriends(currentLevelList, invited, friends)
+{
+    let nextLevelFriends = [];
+    let invitedFriendsNames = invited.map(friend => friend.name);
+    nextLevelFriends.concat(currentLevelList.forEach(friend => {
+        friend.friends.filter(candidat =>
+            invitedFriendsNames.indexOf(candidat) === -1)
+    }));
+
+    return friends.filter(friend => nextLevelFriends.includes(friend.name));
 }
 
 exports.Iterator = Iterator;
