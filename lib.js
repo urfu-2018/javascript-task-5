917ce6647677084f3200c883311cfe46dd8ff985
Nerzull3 <dima3.98@mail.ru>
2018-11-13 11:37:41 +0500

    1st attempt

--- lib.js
+++ lib.js
@@ -1,5 +1,34 @@
 'use strict';
 
+function getGuests(friends, maxLevel) {
+    let temporaryListOfFriends = friends
+        .filter(friend => friend.best)
+        .sort((person1, person2) => person1.name > person2.name);
+    const guests = [...temporaryListOfFriends];
+
+    while (--maxLevel > 0 && temporaryListOfFriends.length > 0) {
+        temporaryListOfFriends = getNotInvitedFriends(
+            temporaryListOfFriends.map(friend => friend.friends),
+            guests
+        )
+            .sort((personName1, personName2) => personName1 > personName2)
+            .map(friendName => friends.find(person => person.name === friendName));
+
+        guests.push(...temporaryListOfFriends);
+    }
+
+    return guests;
+}
+
+function getNotInvitedFriends(currentLevelFriends, guests) {
+    const result = [];
+    for (const listOfFriends of currentLevelFriends) {
+        result.push(...listOfFriends);
+    }
+
+    return result.filter(friendName => guests.every(friend => friendName !== friend.name));
+}
+
 /**
  * Итератор по друзьям
  * @constructor
@@ -7,11 +36,23 @@
  * @param {Filter} filter
  */
 function Iterator(friends, filter) {
-    console.info(friends, filter);
+    if (!(filter instanceof Filter)) {
+        throw new TypeError('Filter does not have type "Filter"');
+    }
+
+    this.guests = getGuests(friends, Infinity).filter(filter.fitsTheCondition);
+
+    this.next = function () {
+        return this.done() ? null : this.guests.shift();
+    };
+
+    this.done = function () {
+        return this.guests.length === 0;
+    };
 }
 
 /**
- * Итератор по друзям с ограничением по кругу
+ * Итератор по друзьям с ограничением по кругу
  * @extends Iterator
  * @constructor
  * @param {Object[]} friends
@@ -19,15 +60,20 @@ function Iterator(friends, filter) {
  * @param {Number} maxLevel – максимальный круг друзей
  */
 function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+    Iterator.call(this, friends, filter);
+    this.guests = getGuests(friends, maxLevel).filter(filter.fitsTheCondition);
 }
+
+LimitedIterator.prototype = Object.create(Iterator.prototype);
 
 /**
  * Фильтр друзей
  * @constructor
  */
 function Filter() {
-    console.info('Filter');
+    this.fitsTheCondition = function () {
+        return true;
+    };
 }
 
 /**
@@ -36,8 +82,12 @@ function Filter() {
  * @constructor
  */
 function MaleFilter() {
-    console.info('MaleFilter');
+    this.fitsTheCondition = function (person) {
+        return person.gender === 'male';
+    };
 }
+
+MaleFilter.prototype = Object.create(Filter.prototype);
 
 /**
  * Фильтр друзей-девушек
@@ -45,8 +95,13 @@ function MaleFilter() {
  * @constructor
  */
 function FemaleFilter() {
-    console.info('FemaleFilter');
+    this.fitsTheCondition = function (person) {
+        return person.gender === 'female';
+    };
 }
+
+FemaleFilter.prototype = Object.create(Filter.prototype);
+
 
 exports.Iterator = Iterator;
 exports.LimitedIterator = LimitedIterator;
