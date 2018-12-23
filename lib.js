0ce55add03a4ac2e0e487159ab6f07f6a7261d9b
JudithRomero <43612189+JudithRomero@users.noreply.github.com>
2018-12-23 22:03:22 +0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,56 +1,92 @@
 'use strict';
 
-/**
- * Итератор по друзьям
- * @constructor
- * @param {Object[]} friends
- * @param {Filter} filter
- */
-function Iterator(friends, filter) {
-    console.info(friends, filter);
+class Filter {
+    is() {
+        return true;
+    }
 }
 
-/**
- * Итератор по друзям с ограничением по кругу
- * @extends Iterator
- * @constructor
- * @param {Object[]} friends
- * @param {Filter} filter
- * @param {Number} maxLevel – максимальный круг друзей
- */
-function LimitedIterator(friends, filter, maxLevel) {
-    console.info(friends, filter, maxLevel);
+class FemaleFilter extends Filter {
+    is(friend) {
+        return ['female', 'bigender', 'transgender', 'genderfluid'].includes(friend.gender);
+    }
 }
 
-/**
- * Фильтр друзей
- * @constructor
- */
-function Filter() {
-    console.info('Filter');
+class MaleFilter extends FemaleFilter {
+    is(friend) {
+        return !super.is(friend);
+    }
 }
 
-/**
- * Фильтр друзей
- * @extends Filter
- * @constructor
- */
-function MaleFilter() {
-    console.info('MaleFilter');
+class Iterator {
+    constructor(friends, filter) {
+        if (filter instanceof Filter) {
+            this.friends = friends;
+            this.filter = filter;
+            this.queue = friends.filter(x => x.best);
+            this.layer = [];
+            this.index = 0;
+            this.depth = 0;
+            this.used = this.queue.map(x => x.name);
+            this.sort();
+        } else {
+            throw new TypeError();
+        }
+    }
+
+    sort() {
+        this.layer = this.queue.filter(x => this.filter.is(x));
+        this.layer.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    done() {
+        if (this.index < this.layer.length) {
+            return false;
+        }
+        if (!this.layer.length) {
+            return true;
+        }
+        this.queue = this.queue.reduce((acc, friend) => {
+            const nextFriends = this.friends
+                .filter(x => friend.friends.includes(x.name))
+                .filter(x => !this.used.includes(x.name));
+            this.used.push(...nextFriends.map(x => x.name));
+
+            return acc.concat(nextFriends);
+        }, []);
+        this.sort();
+        this.depth++;
+        this.index = 0;
+
+        return this.done();
+    }
+
+    next() {
+        console.warn(this.used);
+
+        return this.done() ? null : this.layer[this.index++];
+    }
 }
 
-/**
- * Фильтр друзей-девушек
- * @extends Filter
- * @constructor
- */
-function FemaleFilter() {
-    console.info('FemaleFilter');
+class LimitedIterator extends Iterator {
+    constructor(friends, filter, maxDepth) {
+        super(friends, filter);
+        this.maxDepth = maxDepth;
+    }
+
+    done() {
+        return this.depth >= this.maxDepth || super.done();
+    }
+
+    next() {
+        return this.done() ? null : super.next();
+    }
 }
 
-exports.Iterator = Iterator;
-exports.LimitedIterator = LimitedIterator;
-
-exports.Filter = Filter;
-exports.MaleFilter = MaleFilter;
-exports.FemaleFilter = FemaleFilter;
+module.exports = {
+    Iterator,
+    LimitedIterator,
+    Filter,
+    MaleFilter,
+    FemaleFilter
+};
